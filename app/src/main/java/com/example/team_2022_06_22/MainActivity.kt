705a9eb1f9d5c8523114d56bdd37539e21151166
DwarfPorter <mrvlmor@gmail.com>
2022-06-22 21:25:57 +0300

    toast

--- app/src/main/java/com/example/team_2022_06_22/MainActivity.kt
+++ app/src/main/java/com/example/team_2022_06_22/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.team_2022_06_22
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "Загрузка", Toast.LENGTH_SHORT).show()
     }
 }
